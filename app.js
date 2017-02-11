54c500466c5c0ebd8d50c5afe2f071bade994b7d
不吃斋 <kris.su.asu@gmail.com>
2017-02-10 18:49:11 -0700

    Add files via upload
    add project images

--- app.js
+++ app.js
@@ -38,30 +38,46 @@ var init = function (){
 
 
 		// init projects
+		// start project jumbotron div
 		divs.push("<div class='jumbotron'>");
 
 		divs.push("<h2>" + value.title + " <span class='badge'>" + value.category + "</span></h2>");
 
 		divs.push("<p>" + value.time + "</p>");
-
+		// start skill div
 		divs.push("<div class='breadcrumb'>");
 	    $.each(value.skills, function(index, value){
 			divs.push("<span class='badge' style='background-color: " + colors[index % 5]+ "'>" + value + "</span>");
 		});
+		// end skill div
 		divs.push("</div>");
+
 
 	    divs.push("<div id='"+ value.id + "'><h3>Details</h3></div>");
 
 	
-
+	    // Summary
+	    // start ul
 		divs.push("<h3>Summary</h3><ul class='list-group'>");
 		$.each(value.bullets, function(index, value){
 			divs.push("<li>" + value + "</li>");
 		});
-
+		// end ul
 		divs.push("</ul>");
 
+		// images for each project
+		// start images div
+		divs.push("<div class='row'>");
+		$.each(value.images, function(index, value){
+			divs.push("<div class='col-lg-4 col-md-4 col-sm-4'>");
+			divs.push("<a href='#' class='thumbnail' ><img src='" + value + "' alt='elasticdb'> </a>");
+			divs.push("</div>");
+		});
 
+		// end images div
+		divs.push("</div>")
+
+		// end jumbotron div
 		divs.push("</div>");
 
 	});
@@ -88,94 +104,3 @@ var fillProjectDetails = function(){
 
 
 var colors = ["PaleVioletRed", "Orange", "LightSeaGreen ", "Gold", "SteelBlue"];
-/*
-var vm = {
-
-	projects : [
-		{
-			id: "elasticdb",
-			title : "Elastic DB",
-			time : "Dec 2016 - Feb 2017",
-			category : "Database",
-			bullets : [
-						"Used TPC-W benchmark to simulate primarily shopping (WIPS), browsing (WIPSb) and web-based ordering (WIPSo).",
-			 			"Developed an elastic distributed database using MySQL replication and used it to serve as the backend for a multi-tier web application running TPC-W benchmark.", 
-			 			"Developed a load balancer to route the browsing queries to master and route the ordering queries to slaves in a round-robin manner", 
-			 			"Use backup slaves to guarantee the high availability (HA) of the database in the real-time.",
-			 			"Auto scale out/in the database server based on service level agreement when real-time workload changes.",
-			 			"Monitor the workload/database/operating system, display statistic graph of metrics (workload/queries/resources) using CanvasJS."
-			 		],
-			skills : [
-				"High availability and dynamic scalability of a distributed database", 
-				"Multi-tier web application", 
-				"TPC-W benchmark",
-				"MySQL(replication)",
-				"EasyRules",
-				"JavaScript",
-				"Dstats",
-				"CanvasJS",
-				"Git",
-				"Eclipse",
-				"AWS"
-			],
-			images : ["", "", ""],
-			progress : "",
-			details :[
-				{
-					type : "p",
-					content : "this is a test text for elastic db "
-				}, 
-				{
-					type : "code",
-					content : "public class Solution"
-				},
-				{
-					type : "p",
-					content : "text text text"
-				}
-			]
-
-		},
-		{
-			id: "onlinestore",
-			title : "Online Music Store",
-			time : "Dec 2016 - Jan 2017",
-			category : "Database",
-			bullets : [
-				"Developed UI by using AngularJS and Bootstrap.", 
-				"Implemented backend functions by using Spring MVC and REST Services", 
-				"Designed and Implemented role based user authentication and account management. ", 
-				"Created product module to support CRUD operations, including product searching and sorting, etc.",
-				"Implemented shopping cart and order check out functionality."
-			],
-			skills : [
-				"Spring MVC", 
-				"Spring Data", 
-				"Spring Security",
-				"Spring REST",
-				"Spring Web Flow",
-				"Hibernate",
-				"AngularJS",
-				"JSP",
-				"Bootstrap",
-				"MySQL"
-			],
-			images : ["", "", ""],
-			progress : "",
-			details : [
-				{
-					type : "p",
-					content : "this is a test text for online music store"
-				}, 
-				{
-					type : "code",
-					content : "public class Solution"
-				},
-				{
-					type : "p",
-					content : "text text text"
-				}
-			]
-		},
-	]
-}*/
